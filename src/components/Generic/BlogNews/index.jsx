c3af2a98f1c28178ca02b6342469dc3b9c14e710
Uraimoff <uraimovibrohim5@gmail.com>
2024-08-28 19:06:54 +0500

    Blog part fixed and created components also fetched from github account also added change language

--- src/components/Generic/BlogNews/index.jsx
+++ src/components/Generic/BlogNews/index.jsx
@@ -1,19 +1,47 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "./../../Component/contexts/ThemeContext";
+import LanguageContext from "../../Component/contexts/LanguageContext";
 
-const BlogNews = ({ date, version, description, author, commit }) => {
+const BlogNews = ({
+  date,
+  version,
+  description,
+  author,
+  authorAvatar,
+  commit,
+}) => {
   const { theme } = useContext(ThemeContext);
+  const { language } = useContext(LanguageContext);
   return (
-    <div className={`relative sm:pb-12  flex flex-col items-start w-full p-6 border-l-4 ${theme === 'light' ? 'bg-white border-blue-500 text-black' : 'bg-gray-800 border-blue-400 text-white'}`}>
-    <span className={`absolute left-[-9px] top-6 w-4 h-4 rounded-full ${theme === 'light' ? 'bg-blue-500' : 'bg-blue-400'}`}></span>
-    <p className="text-sm text-gray-400">{date}</p>
-    <h3 className="text-lg font-semibold mt-2">{version}</h3>
-    <p className="text-sm mt-2 text-gray-500">Commit: {commit}</p>
-    <p className="text-sm mt-2">{description}</p>
-    <p className="text-sm text-gray-500 mt-2">Committed by: {author}</p>
-    <a href="#no-more" className="text-blue-400 mt-4">Read more &rarr;</a>
-  </div>
-  
+    <div
+      className={`relative sm:pb-12  flex flex-col items-start w-full p-6 border-l-4 ${
+        theme === "light"
+          ? "bg-white border-blue-500 text-black"
+          : "bg-gray-800 border-blue-400 text-white"
+      }`}
+    >
+      <span
+        className={`absolute left-[-9px] top-6 w-4 h-4 rounded-full ${
+          theme === "light" ? "bg-blue-500" : "bg-blue-400"
+        }`}
+      ></span>
+      <p className="text-sm text-gray-400">{date}</p>
+      <h3 className="text-lg font-semibold mt-2">{version}</h3>
+      <p className="text-sm mt-2 text-gray-500">{language==='en'?'Commit:':language==='ru'?'Коммит:':'Kommit:'} {commit}</p>
+      <p className="text-sm mt-2">{description}</p>
+      <p className="text-sm flex items-center gap-[5px] text-gray-500 mt-2">
+      {language==='en'?'Committed by:':language==='ru'?'Автор коммита:':'Kommit qilgan:'} 
+        <a className="flex items-center gap-[5px]" href={`https://github.com/${author}`}>
+        <div  className="rounded-[50%] mb-[5px] flex justify-center items-center w-[30px] overflow-hidden	">
+          <img className="w-full" src={authorAvatar} alt="" />
+        </div>
+        <p>{author}</p>
+        </a>
+      </p>
+      <a href="#no-more" className="text-blue-400 mt-4">
+      {language==='en'?'Read more':language==='ru'?'Читать дальше':"Ko'proq o'qish"} &rarr;
+      </a>
+    </div>
   );
 };
 
--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import NoData from "../../components/Generic/NoData";
 import BlogNews from "../../components/Generic/BlogNews";
 import axios from "axios";
+import LanguageContext from "../../components/Component/contexts/LanguageContext";
 
 const Blog = () => {
   const [datas, setData] = useState(null);
+  const { language } = useContext(LanguageContext);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,7 +32,7 @@ const Blog = () => {
     <div className="max-w-[52rem] mx-auto px-4 pb-28 sm:px-6 md:px-8 xl:px-12 lg:max-w-6xl">
 {datas ? (
   datas.map((dat, index) => {
-    const formattedDate = new Date(dat.date).toLocaleDateString('en-US', {
+    const formattedDate = new Date(dat.date).toLocaleDateString(language === 'en' ? 'en-US' : language === 'ru' ? 'ru-RU' : 'en-US', {
       year: 'numeric',
       month: 'long',
       day: 'numeric',
@@ -39,6 +41,7 @@ const Blog = () => {
     const formattedVersion = `v${dat.version.substring(0, 3)}`; // Shortened for a more readable version format
 
     const authorName = dat.author ? dat.author.login : 'Unknown';
+    const authorAvatar = dat.author ? dat.author.avatar_url : 'Unknown';
     return (
       <div key={index} className="flex justify-center flex-row ">
         <BlogNews 
@@ -47,6 +50,7 @@ const Blog = () => {
           commit={dat.commit} 
           description={dat.description} 
           author={authorName}
+          authorAvatar={authorAvatar}
         />
       </div>
     );
